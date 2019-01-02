3d93af4d907ba3701a033c00bfd8c72fb7bca53a
Ashnita Bali <ashnita01@gmail.com>
2019-01-02 13:39:47 +0000

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PiglatinComponent } from './piglatin/piglatin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { PiglatinComponent } from './piglatin/piglatin.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
