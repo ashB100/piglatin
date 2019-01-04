55320fe8ddceaf7b9e33557747a68006c4808e60
Ashnita Bali <ashnita01@gmail.com>
2019-01-04 21:16:26 +0000

    Animate routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'piglatin-translator', component: PiglatinComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'piglatin-translator', component: PiglatinComponent, data: { animation: 'piglatin' } },
+  { path: 'about', component: AboutComponent, data: { animation: 'about' } },
   { path: '', redirectTo: '/piglatin-translator', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent }
 ];
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { slideInAnimation } from '../animations';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
+  animations: [slideInAnimation]
 })
 export class NavigationComponent {
 
@@ -17,4 +20,7 @@ export class NavigationComponent {
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
+  getAnimationData(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
 }
