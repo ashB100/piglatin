78ca109a5de41d095c9e3761eee4d33b7378b095
Ashnita Bali <ashnita01@gmail.com>
2019-01-06 18:12:03 +0000

    Attempt to translate from Piglatin to English

--- src/app/piglatin/piglatin.component.ts
+++ src/app/piglatin/piglatin.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { combineLatest, Subject } from 'rxjs/index';
+import { combineLatest, from, of, Subject } from 'rxjs/index';
 import { fromEvent } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 import { Language } from '../language.enum';
@@ -25,8 +25,29 @@ export class PiglatinComponent implements OnInit {
 
   constructor() {
     this.translateFrom$.next(Language.English);
+    const subject = new Subject<number>();
+
+    // I don't understand this. Does the subscribe make subject the observer
+    // with provided next function definitions? Here we have 2 subscriptions
+    // so there are 2 observers?? So the observable is being multicast by
+    // using subject as the observer??
+    subject.subscribe({
+      next: (v) => console.log(`observerA ${v}`)
+    });
+
+    subject.subscribe({
+      next: (v) => console.log(`observerB ${v}`)
+    });
+
+    subject.next(10);
+    subject.next(20);
+
+    const observable = from([1, 2, 3]);
+    observable.subscribe(subject);
   }
+
   ngOnInit() {
+    // Error: property target does not exist on {}
     this.inputString$ = fromEvent(this.inputEl.nativeElement, 'keyup')
       .pipe(
         debounceTime(200),
@@ -36,7 +57,7 @@ export class PiglatinComponent implements OnInit {
     this.translate$ = combineLatest(this.translateFrom$, this.inputString$);
 
     this.translate$.subscribe(value => {
-      console.log(value[0], value[1]);
+      //console.log(value[0], value[1]);
       this.translate(value[0], value[1]);
     });
 
@@ -48,24 +69,56 @@ export class PiglatinComponent implements OnInit {
   }
 
   translate(translateFrom, value: string) {
-    if (/^[aeiou]/i.test(value)) {
-      this.translatedValue = value + 'way';
+    if (translateFrom === Language.English) {
+      if (/^[aeiou]/i.test(value)) {
+        this.translatedValue = value + 'way';
+      } else {
+        this.translatedValue = value
+          .replace(/([bcdfghjklmnpqrstvwxyz]{1,})([aeiou]*)(.*)/i, '$2$3$1ay');
+      }
     } else {
-      this.translatedValue = value
-        .replace(/([bcdfghjklmnpqrstvwxyz]{1,})([aeiou]*)(.*)/i, '$2$3$1ay');
+      // Both words starting with vowel and cons will now start with vowel
+      // to see if it was a vowel, check if ends with way
+
+      // From Piglatin
+
+      // starts with vowel
+      if (/way$/.test(value)) {
+        this.translatedValue = value.replace(/(.*)(way$)/, '$1')
+      } else {
+        let tempValue = value.replace(/(.*)(ay$)/, '$1');
+
+        tempValue = tempValue
+          .split('')
+          .reverse()
+          .join('');
+
+        let constPart = tempValue.match(/[bcdfghjklmnpqrstvwxyz]{1,}/);
+        console.log('Consonant', constPart);
+
+        let rest = tempValue.replace(/([bcdfghjklmnpqrstvwxyz]{1,})([aeiou*].)/, '$2');
+
+        console.log('Rest: ', rest);
+
+      }
+      }
+
+
+    //   if (/^[aeiou]/i.test(value)) {
+    //     this.translatedValue = value.replace(/way/, '');
+    //   } else {
+    //     this.translatedValue = value
+    //     .split('')
+    //     .reverse()
+    //     .join('')
+    //     .replace(/([ay]{1})([bcdfghjklmnpqrstvwxyz]{1,})([aeiou]*)(.*)/, '$2$3$4' );
+    // }
     }
-  }
 
   switchLanguage() {
     const temp = this.translateFrom;
     this.translateFrom = this.translateTo;
     this.translateTo = temp;
     this.translateFrom$.next(this.translateFrom);
-
-    // switch piglatin and inputValue
-    const tempInput = this.inputValue;
-    this.inputEl.nativeElement.value = this.translatedValue;
-    this.inputValue = this.translatedValue;
-    this.translatedValue = tempInput;
   }
 }
