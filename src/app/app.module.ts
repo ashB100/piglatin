ef188758c3bca2e24723998648b317ba15958e62
Ashnita Bali <ashnita01@gmail.com>
2022-04-19 19:16:08 +1200

    backup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/piglatin/piglatin.component.ts
+++ src/app/piglatin/piglatin.component.ts
@@ -4,6 +4,7 @@ import { fromEvent } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 import { Language } from '../language.enum';
 import { filter, map } from 'rxjs/internal/operators';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-piglatin',
@@ -15,19 +16,22 @@ export class PiglatinComponent implements OnInit {
   translateFrom = Language.English;
   translateTo = Language.Piglatin;
 
-  @ViewChild('input') inputEl: ElementRef;
+  // @ViewChild('input') inputEl: ElementRef;
+
+  word = new FormControl();
 
   ngOnInit() {
-    const inputString$ = fromEvent(this.inputEl.nativeElement, 'keyup')
-      .pipe(
-        debounceTime(200)
-      );
-
-    inputString$.subscribe(event => {
-      if (event !== {} ) {
-        this.translate(event.target.value);
-      }
-    });
+    // const inputString$ = fromEvent(this.inputEl.nativeElement, 'keyup')
+    //   .pipe(
+    //     debounceTime(200)
+    //   );
+    //
+    // inputString$.subscribe(event => {
+    //   if (event.target ) {
+    //     this.translate(event.target.value);
+    //   }
+    // });
+    this.word.valueChanges.subscribe(value => this.translate(value))
   }
 
   translate(value: string) {
